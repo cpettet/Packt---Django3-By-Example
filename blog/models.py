249c3155315af6e76f57f135e48363b312893f40
cpettet <cpettet11@gmail.com>
2020-09-21 22:05:47 -0700

    Added support for custom model manager.

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,12 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, 
+                    self).get_queryset() \
+                            .filter(status='published')
+
 class Post(models.Model):
     STATUS_CHOICES = {
         ('draft', 'Draft'),
@@ -21,6 +27,9 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                                 choices=STATUS_CHOICES,
                                 default='draft')
+
+    objects = models.Manager() # The default model manager.
+    published = PublishedManager() # Custom manager.
     
     class Meta:
         ordering = ('-publish',)
